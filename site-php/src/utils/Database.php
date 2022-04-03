72bed4468084b3c0495de28a5cb8798c2da9fa98
Rémi Bernard <remib18@outlook.fr>
2022-04-03 22:11:39 +0200

    Update Database.php

--- site-php/src/utils/Database.php
+++ site-php/src/utils/Database.php
@@ -5,11 +5,10 @@ namespace WebtoonLikeSitePhp\utils;
 class Database
 {
     private static \mysqli $db;
-    private static array $settings = SETTINGS['database'];
 
     public static function getDB(): \mysqli {
         if (!isset(self::$db)) {
-            self::$db = new \mysqli(...self::$settings);
+            self::$db = new \mysqli(...SETTINGS['database']);
         }
         return self::$db;
     }
