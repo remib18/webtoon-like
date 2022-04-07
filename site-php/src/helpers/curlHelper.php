62d22ee4fb9440bd34e5cf19e613c1bd277479d6
Gabey <fluffygabey@gmail.com>
2022-04-07 19:43:14 +0200

    implement httpGet

--- site-php/src/helpers/curlHelper.php
+++ site-php/src/helpers/curlHelper.php
@@ -1,17 +1,41 @@
 <?php
 
-namespace WebtoonLike\Site\src\helpers;
+namespace WebtoonLike\Site\helpers;
 
 class curlHelper {
-    public static function httpGet(): array {
-        
 
-        return [];
+    /**
+     * Retourne la reponse d'une requete GET.
+     *
+     * @param array $params
+     * @return array
+     */
+    public static function httpGet(array $params): array
+    {
+        $url = $params['url'];
+        $curlSession = curl_init($url);
+            curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+            // if a userAgent is specified, use it.
+            if(isset($params['userAgent']))
+            {
+                curl_setopt($curlSession, CURLOPT_HTTPHEADER, $params['userAgent']);
+            }
+
+            // use while testing
+            // curl_setopt($curlSession, CURLOPT_SSL_VERIFYPEER, false);
+            // curl_setopt($curlSession, CURLOPT_SSL_VERIFYHOST, false);
+
+            $response = curl_exec($curlSession);
+            $decodedResponse = json_decode( $response, true );
+            $responseCode = curl_getinfo($curlSession, CURLINFO_HTTP_CODE);
+        curl_close($curlSession);
+
+        return ['httpCode' => $responseCode, 'response' => $decodedResponse];
     }
 
     public static function httpPost(): array {
 
-        
+
         return [];
     }
 }
