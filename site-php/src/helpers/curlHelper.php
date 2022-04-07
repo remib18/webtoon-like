a9b914d1f969d1be7a3d1e7f10d45f1d88afc77a
Gabey <fluffygabey@gmail.com>
2022-04-07 18:13:11 +0200

    Create curlHelper.php

---
+++ site-php/src/helpers/curlHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WebtoonLike\Site\src\helpers;
+
+class curlHelper {
+    public static function httpGet(): array {
+        
+
+        return [];
+    }
+
+    public static function httpPost(): array {
+
+        
+        return [];
+    }
+}
