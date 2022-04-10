6902daf00826b7b37b642067afe48c0cc5f24109
Gabey <fluffygabey@gmail.com>
2022-04-10 22:37:31 +0200

    Revert "Fixing DB initialiser"
    This reverts commit dbc06485

--- sql/init/init-db_v0.sql
+++ sql/init/init-db_v0.sql
@@ -1,66 +1,73 @@
-CREATE DATABASE webtoonlike CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS webtoonlike CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE webtoonlike;
 
-CREATE TABLE Users (
+CREATE TABLE User (
     userID BIGINT NOT NULL AUTO_INCREMENT,
     username VARCHAR(32),
     email VARCHAR(256),
     registeredAt DATETIME,
     PRIMARY KEY (userID)
-)  ENGINE=INNODB;
+)  ENGINE=INNODB; 
 
 CREATE TABLE Report (
     reportID BIGINT NOT NULL AUTO_INCREMENT,
     type INT,
     userID BIGINT NOT NULL,
     PRIMARY KEY (reportID),
-    FOREIGN KEY (userID) REFERENCES Users(userID)
-)  ENGINE=INNODB;
+    FOREIGN KEY (userID) REFERENCES User(userID)
+)  ENGINE=INNODB; 
 
 CREATE TABLE TranslationProposition (
     translationPropositionID BIGINT NOT NULL AUTO_INCREMENT,
     proposedTranslation TEXT,
     PRIMARY KEY (translationPropositionID)
-)  ENGINE=INNODB;
+)  ENGINE=INNODB; 
 
-CREATE TABLE Webtoons (
+CREATE TABLE Propose (
+    translationPropositionID BIGINT NOT NULL,
+    userID BIGINT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES User(userID),
+    FOREIGN KEY (translationPropositionID) REFERENCES TranslationProposition(translationPropositionID)
+)  ENGINE=INNODB; 
+
+CREATE TABLE Webtoon (
     webtoonID BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256),
     author VARCHAR(128),
     description TEXT,
     PRIMARY KEY (webtoonID)
-)  ENGINE=INNODB;
+)  ENGINE=INNODB; 
 
 CREATE TABLE Chapter (
-     chapterID BIGINT NOT NULL AUTO_INCREMENT,
-     number INT,
-     title VARCHAR(256),
-     webtoonID BIGINT NOT NULL,
-     PRIMARY KEY (chapterID),
-     FOREIGN KEY (webtoonID) REFERENCES Webtoons(webtoonID)
-)  ENGINE=INNODB;
+    chapterID BIGINT NOT NULL AUTO_INCREMENT,
+    number INT,
+    title VARCHAR(256),
+    webtoonID BIGINT NOT NULL,
+    PRIMARY KEY (chapterID),
+    FOREIGN KEY (webtoonID) REFERENCES Webtoon(webtoonID)
+)  ENGINE=INNODB; 
 
 CREATE TABLE Image (
-     imageID BIGINT NOT NULL AUTO_INCREMENT,
-     `index` INT,
-     path VARCHAR(256),
-     chapterID BIGINT NOT NULL,
-     PRIMARY KEY (imageID),
-     FOREIGN KEY (chapterID) REFERENCES Chapter(chapterID)
-)  ENGINE=INNODB;
+    imageID BIGINT NOT NULL AUTO_INCREMENT,
+    `index` INT,
+    path VARCHAR(256),
+    chapterID BIGINT NOT NULL,
+    PRIMARY KEY (imageID),
+    FOREIGN KEY (chapterID) REFERENCES Chapter(chapterID)
+)  ENGINE=INNODB; 
 
 CREATE TABLE CellPosition (
     cellPositionID BIGINT NOT NULL AUTO_INCREMENT,
     xAxis INT,
     yAxis INT,
     PRIMARY KEY (cellPositionID)
-)  ENGINE=INNODB;
+)  ENGINE=INNODB; 
 
 CREATE TABLE AvailableLanguage (
     languageID VARCHAR(256) NOT NULL,
     languageName VARCHAR(256) NOT NULL,
     PRIMARY KEY (languageID)
-)  ENGINE=INNODB;
+)  ENGINE=INNODB; 
 
 CREATE TABLE Cell (
     cellID BIGINT NOT NULL AUTO_INCREMENT,
@@ -72,4 +79,4 @@ CREATE TABLE Cell (
     FOREIGN KEY (imageID) REFERENCES Image(imageID),
     FOREIGN KEY (cellPositionID) REFERENCES CellPosition(cellPositionID),
     FOREIGN KEY (languageID) REFERENCES AvailableLanguage(languageID)
-)ENGINE=INNODB;
+)ENGINE=INNODB; 
