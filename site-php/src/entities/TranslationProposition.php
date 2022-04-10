14a36f89edb13a237034f0f1c95a36ca22a6f317
Gabey <fluffygabey@gmail.com>
2022-04-10 22:25:04 +0200

    implement translationProposition

--- site-php/src/entities/TranslationProposition.php
+++ site-php/src/entities/TranslationProposition.php
@@ -7,9 +7,48 @@ use JetBrains\PhpStorm\Pure;
 
 class TranslationProposition implements EntityInterface
 {
+
+    public function __construct(
+        private int $id,
+        private string $proposedTranslation,
+    ) {}
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProposedTranslation(): string
+    {
+        return $this->proposedTranslation;
+    }
+
+    /**
+     * @param string $proposedTranslation
+     */
+    public function setProposedTranslation(string $proposedTranslation): void
+    {
+        $this->proposedTranslation = $proposedTranslation;
+    }
+
     public function __toArray(): array {
         return [
-
+            'id' => $this->id,
+            'proposedTranslation' => $this->proposedTranslation
         ];
     }
 
