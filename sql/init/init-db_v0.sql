b435cc5b307f3e270649abee85ae8db9ae868e24
Gabey <fluffygabey@gmail.com>
2022-04-10 22:29:35 +0200

    Update init-db_v0.sql

--- sql/init/init-db_v0.sql
+++ sql/init/init-db_v0.sql
@@ -42,7 +42,7 @@ CREATE TABLE Chapter (
 
 CREATE TABLE Image (
      imageID BIGINT NOT NULL AUTO_INCREMENT,
-     position INT,
+     `index` INT,
      path VARCHAR(256),
      chapterID BIGINT NOT NULL,
      PRIMARY KEY (imageID),
