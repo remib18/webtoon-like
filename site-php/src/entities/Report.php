1b43e512220c47946774aa8364bfd2a04d930336
Gabey <fluffygabey@gmail.com>
2022-04-11 14:29:13 +0200

    implement report interface

--- site-php/src/entities/Report.php
+++ site-php/src/entities/Report.php
@@ -7,9 +7,66 @@ use JetBrains\PhpStorm\Pure;
 
 class Report implements EntityInterface
 {
+    public function __construct(
+        private int $id,
+        private int $type,
+        private int $userId
+    ){}
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     */
+    public function setType(int $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    #[ArrayShape(['id' => "int", 'type' => "int", 'userId' => "int"])]
     public function __toArray(): array {
         return [
-
+            'id' => $this->id,
+            'type' => $this->type,
+            'userId' => $this->userId
         ];
     }
 
