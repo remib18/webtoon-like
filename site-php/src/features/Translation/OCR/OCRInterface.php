9954978cb2a9bfc6de83f87ca59837a86346b446
Rémi Bernard <remib18@outlook.fr>
2022-04-11 16:38:55 +0200

    Updating OCR Interface

--- site-php/src/features/Translation/OCR/OCRInterface.php
+++ site-php/src/features/Translation/OCR/OCRInterface.php
@@ -3,13 +3,20 @@
 namespace WebtoonLike\Site\features\Translation\OCR;
 
 use WebtoonLike\Site\entities\Image;
-use WebtoonLike\Site\features\Translation\entities;
+use WebtoonLike\Site\features\Translation\Result\Result;
 
 interface OCRInterface
 {
 
+    /**
+     * @param Image $image L'image à charger
+     * @return $this
+     */
     public function registerImage(Image $image): OCRInterface;
 
-    public function runOCR(): Result;
+    /**
+     * @return array<Result>
+     */
+    public function runOCR(): array;
 
 }
