a13c9410da653a882c58db7ce62203027521da52
Rémi Bernard <remib18@outlook.fr>
2022-04-12 19:28:10 +0200

    Updating EntityInterface

--- site-php/src/entities/Webtoon.php
+++ site-php/src/entities/Webtoon.php
@@ -7,15 +7,23 @@ use JetBrains\PhpStorm\ArrayShape;
 class Webtoon implements EntityInterface
 {
 
+    private array $fieldsToSave = [];
+
     private ?int $id;
+    private string $name;
+    private string $author;
+    private string $description;
 
     public function __construct(
         ?int $webtoonID,
-        private string $name,
-        private string $author,
-        private string $description
+        string $name,
+        string $author,
+        string $description
     ) {
         $this->id = $webtoonID;
+        $this->setName($name);
+        $this->setAuthor($author);
+        $this->setDescription($description);
     }
 
     /**
@@ -39,6 +47,7 @@ class Webtoon implements EntityInterface
      */
     public function setAuthor(string $author): void
     {
+        $this->fieldsToSave['author'] = $author;
         $this->author = $author;
     }
 
@@ -55,6 +64,7 @@ class Webtoon implements EntityInterface
      */
     public function setDescription(string $description): void
     {
+        $this->fieldsToSave['description'] = $description;
         $this->description = $description;
     }
 
@@ -71,26 +81,77 @@ class Webtoon implements EntityInterface
      */
     public function setName(string $name): void
     {
+        $this->fieldsToSave['name'] = $name;
         $this->name = $name;
     }
 
-    #[ArrayShape(['id' => "int", 'name' => "string", 'author' => "string", 'description' => "string"])]
+    /**
+     * @inheritDoc
+     */
+    #[ArrayShape([
+        'webtoonID' => "int",
+        'name' => "string",
+        'author' => "string",
+        'description' => "string"
+    ])]
     public function __toArray(): array
     {
         return [
-            'id' => $this->id,
+            'webtoonID' => $this->id,
             'name' => $this->name,
             'author' => $this->author,
             'description' => $this->description
         ];
     }
 
-    public static function getColumnsKeys(): array {
+    /**
+     * @inheritDoc
+     */
+    public static function getColumnsKeys(): array
+    {
+        return ['webtoonID', 'name', 'author', 'description'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getIdentifiers(): array
+    {
+        return ['webtoonID'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFieldsToSave(): array
+    {
+        return $this->fieldsToSave;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function AllFieldsSaved(): void
+    {
+        $this->fieldsToSave = [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    #[ArrayShape([
+        'webtoonID' => "string",
+        'name' => "string",
+        'author' => "string",
+        'description' => "string"
+    ])]
+    public static function getTypes(): array
+    {
         return [
-            'webtoonID',
-            'name',
-            'author',
-            'description'
+            'webtoonID' => 'integer',
+            'name' => 'string',
+            'author' => 'string',
+            'description' => 'string'
         ];
     }
 }
