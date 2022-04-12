3ba291234676641e4ada8e727a7a9391cc40271b
Rémi Bernard <remib18@outlook.fr>
2022-04-12 15:05:26 +0200

    Update Database.php
    - Adding PHPDoc
    - Adding static method to transform a result to an array of objects

--- site-php/src/utils/Database.php
+++ site-php/src/utils/Database.php
@@ -3,17 +3,39 @@
 namespace WebtoonLike\Site\utils;
 
 use mysqli;
+use WebtoonLike\Site\entities\EntityInterface;
 use WebtoonLike\Site\Settings;
 
 class Database
 {
     private static mysqli $db;
 
+    /**
+     * Obtenir l'instance de la base de données
+     *
+     * @return mysqli
+     */
     public static function getDB(): mysqli {
         if (!isset(self::$db)) {
             self::$db = new mysqli(...Settings::get('database'));
         }
         return self::$db;
+    }
+
+    /**
+     * Transforme une réponse mysqli en tableau d'objets
+     *
+     * @param array $response Réponse mysqli avec un fetchAll assoc
+     * @param string $class Classe représentant les objects
+     *
+     * @return array<EntityInterface>
+     */
+    public static function responseToObjects(array $response, string $class): array {
+        $res = [];
+        foreach ($response as $item) {
+            $res[] = new $class(...$item);
+        }
+        return $res;
     }
 
 }
