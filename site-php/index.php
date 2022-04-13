5bf9f361ae0f70c00f3f3b7feee194a24ed208d2
Gabey <fluffygabey@gmail.com>
2022-04-13 20:41:45 +0200

    error handling production/testing

--- site-php/index.php
+++ site-php/index.php
@@ -1,10 +1,3 @@
 <?php
 
-use WebtoonLike\Site\Settings;
-
-if( Settings::get('production') == false ) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-}
-
 require 'src/index.php';
