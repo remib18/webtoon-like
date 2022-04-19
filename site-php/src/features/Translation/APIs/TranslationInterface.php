495e9ce9372176d44ea55d919dc1b4d847dd42ac
Rémi Bernard <remib18@outlook.fr>
2022-04-19 22:16:09 +0200

    Update to use Database

--- site-php/src/features/Translation/APIs/TranslationInterface.php
+++ site-php/src/features/Translation/APIs/TranslationInterface.php
@@ -3,29 +3,32 @@
 namespace WebtoonLike\Site\features\Translation\APIs;
 
 use WebtoonLike\Site\entities\Language;
+use WebtoonLike\Site\features\Translation\Result\Bloc;
 
 interface TranslationInterface
 {
+    // TODO: Remplacer string $text by Block $block
+
     /**
      * Traduit un texte
      *
-     * @param string $text Texte à traduire
+     * @param Bloc $bloc Bloc à traduire
      * @param Language $source Langue d'origine
      * @param Language $target Langue vers laquelle traduire
      *
      * @return string Traduction
      */
-    public static function translate(string $text, Language $source, Language $target): string;
+    public static function translate(Bloc $bloc, Language $source, Language $target): string;
 
     /**
      * Traduit plusieurs textes issus du même langage.
      *
-     * @param array<string> $texts Liste des textes à traduire
+     * @param Bloc[] $blocs Liste des blocs à traduire
      * @param Language $source Langue d'origine
      * @param Language $target Langue vers laquelle traduire
      *
      * @return array<string> Traductions (l'ordre est conservé)
      */
-    public static function translateMany(array $texts, Language $source, Language $target): array;
+    public static function translateMany(array $blocs, Language $source, Language $target): array;
 
 }
