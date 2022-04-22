28511aab31a0d77d5ab385f5f42055007cc28282
Gabey <fluffygabey@gmail.com>
2022-04-22 20:09:15 +0200

    Update Register.php

--- site-php/src/handler/Register.php
+++ site-php/src/handler/Register.php
@@ -1,1 +1,19 @@
 <?php
+
+    namespace WebtoonLike\Site\handler;
+
+    use WebtoonLike\Site\core\Authentication;
+    use WebtoonLike\Site\core\Router;
+
+    if ( !isset($_POST['password'], $_POST['confirmation_password'], $_POST['email'], $_POST['username']) ) {
+        Router::redirect('', '400', [
+            'registerError' => 'Un ou plusieurs champs ne sont pas définis.', 'action' => 'register'
+        ]);
+    }
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmation_password = $_POST['confirmation_password'];
+
+    Authentication::register($username, $email, $password, $confirmation_password);
