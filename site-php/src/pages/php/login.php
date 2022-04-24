7e0ff7b6402061638c271c5154ea2634941fb4a9
Gabey <fluffygabey@gmail.com>
2022-04-25 00:13:13 +0200

    made field required

--- site-php/src/pages/php/login.php
+++ site-php/src/pages/php/login.php
@@ -2,9 +2,9 @@
 
 <section aria-describedby="#s1-title" id="app" xmlns:input="http://www.w3.org/1999/html">
     <h2 id="s1-title">Login</h2>
-    <form action="/@login" method="post">
-        <input type="email" name="email">
-        <input type="password" name="password">
+    <form action="/@login" method="post" required>
+        <input type="email" name="email" required>
+        <input type="password" name="password" required>
         <input type="submit" value="Connexion">
     </form>
 </section>
--- site-php/src/pages/php/register.php
+++ site-php/src/pages/php/register.php
@@ -3,10 +3,10 @@
 <section aria-describedby="#s1-title" id="app" xmlns:input="http://www.w3.org/1999/html">
     <h2 id="s1-title">Register</h2>
     <form action="/@register" method="post">
-        <input type="text" name="username">
-        <input type="email" name="email">
-        <input type="password" name="password">
-        <input type="password" name="confirmation_password">
+        <input type="text" name="username" required>
+        <input type="email" name="email" required>
+        <input type="password" name="password" required>
+        <input type="password" name="confirmation_password" required>
         <input type="submit" value="Connexion">
     </form>
 </section>
