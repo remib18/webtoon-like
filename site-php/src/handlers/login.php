42c0b1041500f9d3db2a06280217091a38d718a4
Gabey <fluffygabey@gmail.com>
2022-04-24 20:17:50 +0200

    Fixed $_POST value.

--- site-php/src/handlers/login.php
+++ site-php/src/handlers/login.php
@@ -5,8 +5,8 @@ namespace WebtoonLike\Site\handler;
 use WebtoonLike\Site\core\Authentication;
 use WebtoonLike\Site\core\Router;
 
-if ( !isset($_POST['email'], $_POST['username']) ) {
-    Router::redirect('/#login');
+if ( !isset($_POST['email'], $_POST['password']) ) {
+    Router::redirect('/login', 301, ['msg' => 'Vous devez remplir TOUS les champs.']);
 }
 
 $email = $_POST['email'];
