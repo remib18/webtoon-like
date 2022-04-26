0b25653ea533b56d86f24f1c739d7da676ab5ca7
Gabey <fluffygabey@gmail.com>
2022-04-26 23:10:16 +0200

    data validation draft (has to be tested)

--- site-php/src/utils/DataTesting/DataVerification.php
+++ site-php/src/utils/DataTesting/DataVerification.php
@@ -30,22 +30,87 @@ class DataVerification {
     }
 
     public function verifyString(string $str): bool {
+        if($this->field->getMinLength() !== null && strlen($str) < $this->field->getMinLength()) {
+            return false;
+        }
 
+        if($this->field->getMaxLength() !== null && strlen($str) > $this->field->getMaxLength()) {
+            return false;
+        }
+
+        if($this->field->getNullable() === false && empty($str) === true) {
+            return false;
+        }
+
+        if($this->field->getRegex() !== null && preg_match($this->field->getRegex(),$str) === 0) {
+            return false;
+        }
+
+        return true;
     }
 
     public function verifyEmail(string $email): bool {
+        if($this->field->getMinLength() !== null && strlen($email) < $this->field->getMinLength()) {
+            return false;
+        }
+
+        if($this->field->getMaxLength() !== null && strlen($email) > $this->field->getMaxLength()) {
+            return false;
+        }
+
+        if($this->field->getNullable() === false && empty($email) === true) {
+            return false;
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+        if($this->field->getRegex() !== null && preg_match($this->field->getRegex(), $email) === 0) {
+            return false;
+        }
+
         return true;
     }
 
     public function verifyInt(int $int): bool {
+
+        if($this->field->getNullable() === false && empty($int) === true) {
+            return false;
+        }
+
+        if($this->field->getRegex() !== null && preg_match($this->field->getRegex(),$int) === 0) {
+            return false;
+        }
+
         return true;
     }
 
     public function verifyFloat(float $float): bool {
+
+        if($this->field->getNullable() === false && empty($float) === true) {
+            return false;
+        }
+
+        if($this->field->getRegex() !== null && preg_match($this->field->getRegex(),$float) === 0) {
+            return false;
+        }
+
         return true;
     }
 
     public function verifyDate(Datetime $date): bool {
+
+        if($this->field->getNullable() === false && empty($float) === true) {
+            return false;
+        }
+
+        if($this->field->getRegex() !== null && preg_match($this->field->getRegex(),$float) === 0) {
+            return false;
+        }
+
+        // Todo: validate the date using its format.
+
         return true;
     }
 
