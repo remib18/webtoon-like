1638b34dfe71c6b659ebf1f39e53657b9c7fc56c
Gabey <fluffygabey@gmail.com>
2022-04-30 23:41:58 +0200

    Update LoginTokenController.php

--- site-php/src/controller/LoginTokenController.php
+++ site-php/src/controller/LoginTokenController.php
@@ -7,16 +7,29 @@ use WebtoonLike\Site\utils\Database;
 
 class LoginTokenController
 {
+    /**
+     * @param string $token
+     * @return LoginToken|null
+     */
     public static function getByToken(string $token): ?LoginToken
     {
         return Database::getFirst('LoginToken', LoginToken::class, '*', ['token' => "token = '$token'"]);
     }
 
+    /**
+     * @param LoginToken $entity
+     * @return bool
+     * @throws \WebtoonLike\Site\entities\NoIdOverwritingException
+     */
     public static function create(LoginToken &$entity): bool
     {
         return Database::create('LoginToken', $entity);
     }
 
+    /**
+     * @param LoginToken $entity
+     * @return bool
+     */
     public static function remove(LoginToken $entity): bool
     {
         return Database::remove('LoginToken', $entity);
