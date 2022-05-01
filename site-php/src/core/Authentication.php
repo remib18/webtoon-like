d75d9ab6af429005fef6b91ef8c8cd3e44249727
Gabey <fluffygabey@gmail.com>
2022-05-01 12:58:36 +0200

    token are now unique with absolute certainty,

--- site-php/src/core/Authentication.php
+++ site-php/src/core/Authentication.php
@@ -136,7 +136,7 @@ class Authentication {
 
             if( $rememberMe ) {
                 $lifeSpan = time() + 86400 * 14;
-                $token = strtoupper(md5(openssl_random_pseudo_bytes(64)));
+                $token = strtoupper(md5(openssl_random_pseudo_bytes(64) . time()));
 
                 $res = LoginTokenController::create(new LoginToken($token, $lifeSpan, $user->getId(), false));
                 if($res !== false ) {
