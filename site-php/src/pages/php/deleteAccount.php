2a9a9950609b0aeda0c098eaada20caac12b959d
Rémi Bernard <remib18@outlook.fr>
2022-05-04 17:17:46 +0200

    Improving design

--- site-php/src/pages/php/deleteAccount.php
+++ site-php/src/pages/php/deleteAccount.php
@@ -1,6 +1,6 @@
 <?php require dirname(__DIR__, 2) . '/components/header.php'; ?>
 
-<section aria-describedby="#s1-title" id="app" xmlns:input="http://www.w3.org/1999/html">
+<section aria-describedby="#s1-title" id="app">
 
     <h2 id="s1-title">Supprimer votre compte:</h2>
     <?= WebtoonLike\Site\pageManager\deleteAccountManager::getErrors() ?>
@@ -13,14 +13,15 @@
         </p>
         <br>
         <label for="password">Validez votre mot de passe:
-            <input type="password" name="password" id="password" placeholder="mon mot de passe" required>
+            <input type="password" name="password" id="password" placeholder="Mon mot de passe" required
+                   class="large danger">
         </label>
 
         <label for="deleteAccount">Je comprend que cette operation est irreversible
             <input type="checkbox" name="deleteAccount" id="deleteAccount">
         </label>
 
-        <input type="submit" value="Supprimer mon compte.">
+        <input type="submit" value="Supprimer mon compte." class="danger">
     </form>
 
 </section>
