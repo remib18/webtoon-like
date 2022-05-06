486b036e8be1800b88d34e8c0d5c34022d0feaaf
Gabey <fluffygabey@gmail.com>
2022-05-07 01:42:44 +0200

    Revert "Merge branch 'yacine/populating-data' of https://github.com/remib18/webtoon-like into yacine/populating-data"
    This reverts commit ec169b184563fdb2e757975b3390c7f773dc37b9, reversing
    changes made to 62db1cfa3cd8e6e5e7c7dfb47eb8e692c3d30df1.

--- site-php/src/handlers/import-chapter.php
+++ site-php/src/handlers/import-chapter.php
@@ -1,13 +1,1 @@
 <?php
-
-
-use WebtoonLike\Site\core\Router;
-use WebtoonLike\Site\pageManager\ImportManager;
-
-if(!(isset($_POST['chapter-x-title'], $_POST['chapter-x-number'],$_POST['id'])
-    && !(empty($_POST['chapter-x-title']) || empty($_POST['chapter-x-number'] || empty($_POST['id']))))
-){
-    Router::redirect('/error', 301, ['msg' => 'Aucun chapitre soumis ou champ manquant']);
-}
-importManager::saveChapter();
-
