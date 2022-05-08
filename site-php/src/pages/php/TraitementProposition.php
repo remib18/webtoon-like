992718fd206ad6651df2678bf033c5bce1a82302
Gabey <fluffygabey@gmail.com>
2022-05-08 21:41:20 +0200

    removed unused.

--- site-php/src/pages/php/TraitementProposition.php
+++
@@ -1,7 +0,0 @@
-<?php
-use WebtoonLike\Site\features\Translation;
-
-if(isset($_GET['proposition']) && isset($_GET['proposition']) && isset($_GET['proposition'])){
-    ::setContent(string $content);
-
-}
